e14ae7bc1546e4312020576593305517bfbdc851
davidsilva02 <davidsilva200210.ds@gmail.com>
2023-11-01 18:36:32 +0000

    update list with click

--- app.js
+++ app.js
@@ -4,8 +4,29 @@ let a={
   'cube':true
 };
 
+let list_component;
 let string_value = "Lista de Palavras:";
-
+AFRAME.registerComponent('list', {
+  init: function () {
+    // Wait for the scene to load before setting the value
+    list_component=this.el
+    this.el.addEventListener('loaded', () => {
+        // Concatenate string_value with all keys from the 'a' object
+        let concatenatedString = string_value + "\n";
+        for (const key in a) {
+            if (a[key]) {
+                concatenatedString += "o - ";
+            }
+            else {
+                concatenatedString += "x - ";
+            }
+            concatenatedString += key + "\n";
+        }
+        // Set the concatenated string as the value of the <a-text> element
+        this.el.setAttribute('value', concatenatedString);
+    });
+}
+});
 AFRAME.registerComponent('button', {
     init: function () {
       var el = this.el;
@@ -24,7 +45,20 @@ AFRAME.registerComponent('button', {
 
       el.addEventListener('click',function(){
           a[el.getAttribute('value')]=false;
-          console.log(a);
+
+          //update list
+          let concatenatedString = string_value + "\n";
+          for (const key in a) {
+              if (a[key]) {
+                  concatenatedString += "o - ";
+              }
+              else {
+                  concatenatedString += "x - ";
+              }
+              concatenatedString += key + "\n";
+          }
+
+          list_component.setAttribute('value', concatenatedString);
 
           if(el.getAttribute('opacity')==0.5) el.setAttribute("opacity",1.0);             
           else el.setAttribute("opacity",0.5);
