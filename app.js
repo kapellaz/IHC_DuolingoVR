44faf1c01a4836f3563f282a6fbc1671396de3a6
samu <sebmachado2002@gmail.com>
2023-11-04 22:53:19 +0000

    primeira ação implementada

--- app.js
+++ app.js
@@ -3,13 +3,11 @@ let a={
   'engine':true,
   'wheel':true,
   'workbench':true,
-  'car': true,
-  'canister': true,
 };
 
 
 let acoes = { // pressupomos acoes de 2 objetos apenas
-  'change oil':['oil','car'], //removido da lista quando encontrado até
+  'change oil':['canister','car'], //removido da lista quando encontrado até
 }
 
 let count_found = 0;
@@ -41,6 +39,9 @@ AFRAME.registerComponent('list', {
         for (let key in acoes) {
           let percentage = (1-(acoes[key].length/2))*100;
           concatenatedString += percentage.toString() + "%: " + key + "\n";
+          for (let i = 0; i < acoes[key].length; i++) {
+            concatenatedString += ' -> ' + acoes[key][i] + '\n';
+          }
         }
         // Set the concatenated string as the value of the <a-text> element
         this.el.setAttribute('value', concatenatedString);
@@ -53,9 +54,10 @@ AFRAME.registerComponent('button', {
     init: function () {
       var el = this.el;
       cursor = document.getElementById('cursor');
+      let objeto = el.getAttribute('value');
 
       el.addEventListener('mouseenter', function () {
-        if (a[el.getAttribute('value')]==true) cursor.setAttribute('color', "green");
+        if (a[objeto]==true) cursor.setAttribute('color', "green");
         else cursor.setAttribute('color', "cyan");
       });
       
@@ -65,32 +67,49 @@ AFRAME.registerComponent('button', {
 
 
       el.addEventListener('click',function(){
-        if(a[el.getAttribute('value')]==true){
-          a[el.getAttribute('value')]=false;
-          console.log(a);
+        let in_action = false;
+        if(a[objeto]==true){ //verificar se é objeto
+          a[objeto]=false;
           count_found++;
           if (count_found == Object.keys(a).length){
             all_found = true;
-          }
-
-          //update list
-          let concatenatedString = string_value + "\n";
-          for (const key in a) {
-              if (a[key]) {
-                  concatenatedString += "o - ";
-              }
-              else {
-                  concatenatedString += "x - ";
-              }
-              concatenatedString += key + "\n";
-          }
-          concatenatedString += "\n---------------\n\n";
-          for (let key in acoes) {
-            let percentage = (1-(acoes[key].length/2))*100;
-            concatenatedString += percentage.toString() + "%: " + key + "\n";
-          }
-          list_component.setAttribute('value', concatenatedString);
+          } 
         }
+        else {
+          for (let key in acoes) {
+            if (acoes[key].includes(objeto)) {
+              acoes[key] = acoes[key].filter(item => item != objeto);
+              in_action = true;
+              if (acoes[key].length === 0) {
+                count_done++;
+                if (count_done === Object.keys(acoes).length) {
+                  all_done = true;
+                }
+              }
+            }
+          }
+        }
+        if (a[objeto]==false || in_action) {
+          let concatenatedString = string_value + "\n";
+            for (const key in a) {
+                if (a[key]) {
+                    concatenatedString += "o - ";
+                }
+                else {
+                    concatenatedString += "x - ";
+                }
+                concatenatedString += key + "\n";
+            }
+            concatenatedString += "\n---------------\n\n";
+            for (let key in acoes) {
+              let percentage = (1-(acoes[key].length/2))*100;
+              concatenatedString += percentage.toString() + "%: " + key + "\n";
+              for (let i = 0; i < acoes[key].length; i++) {
+                concatenatedString += ' -> ' + acoes[key][i] + '\n';
+              }
+            }
+            list_component.setAttribute('value', concatenatedString);
+          }
       })
     }
   });
