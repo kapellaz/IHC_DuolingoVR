bd3ba3e6e4fac9b783d03d2c8ca2e9ad8256ef7f
samu <sebmachado2002@gmail.com>
2023-11-05 16:06:42 +0000

    removida redundancia no codigo + correções

--- scenario.js
+++ scenario.js
@@ -1,121 +1,104 @@
-
-
-
-let all_selected = [];
-
-let count_found = 0;
-let count_done = 0;
-let all_found = false;
-let all_done = false;
-
+let all_selected = []; // tudo o que foi selecionado
+let count_found = 0;   //       objetos encontrados
+let count_done = 0;    //       ações realizadas
+let all_found = false; // todos objetos encontrados
+let all_done = false;  // todas ações realizadas
 let list_component;
 let cursor;
 
+function updateList() {
+  let concatenatedString = "Objetos:" + "\n";
+  for (const key in a) {
+    if (a[key]) {
+      concatenatedString += "o - "; 
+    } else {
+      concatenatedString += "x - ";
+    }
+    concatenatedString += key + "\n";
+  }
+  concatenatedString += "---------------\n";
+  concatenatedString += "Tarefas:\n";
+  for (let key in acoes) {
+    let percentage = (1 - (acoes[key].length / 2)) * 100;
+    concatenatedString += percentage.toString() + "%: " + key + "\n";
+    for (let i = 0; i < acoes[key].length; i++) {
+      concatenatedString += ' -> ' + acoes[key][i] + '\n';
+    }
+  }
+  list_component.setAttribute('value', concatenatedString);
+}
 
 AFRAME.registerComponent('list', {
   init: function () {
-    // Wait for the scene to load before setting the value
     list_component = this.el;
     this.el.addEventListener('loaded', () => {
-        // Concatenate string_value with all keys from the 'a' object
-        let concatenatedString = "Objetos:" + "\n";
-        for (const key in a) {
-            if (a[key]) {
-                concatenatedString += "o - ";
-            }
-            else {
-                concatenatedString += "x - ";
-            }
-            concatenatedString += key + "\n";
-        }
-        concatenatedString += "---------------\n";
-        concatenatedString += "Tarefas:\n"
-        for (let key in acoes) {
-          let percentage = (1-(acoes[key].length/2))*100;
-          concatenatedString += percentage.toString() + "%: " + key + "\n";
-          for (let i = 0; i < acoes[key].length; i++) {
-            concatenatedString += ' -> ' + acoes[key][i] + '\n';
-          }
-        }
-        // Set the concatenated string as the value of the <a-text> element
-        this.el.setAttribute('value', concatenatedString);
+      updateList();
     });
-}
+  }
 });
 
-
 AFRAME.registerComponent('button', {
-    init: function () {
-      var el = this.el;
-      cursor = document.getElementById('cursor');
-      let objeto = el.getAttribute('value');
+  init: function () {
+    var el = this.el;
+    cursor = document.getElementById('cursor');
+    let objeto = el.getAttribute('value');
 
-      el.addEventListener('mouseenter', function () {
-        if (!all_selected.includes(objeto)) cursor.setAttribute('color', "green");
-        else cursor.setAttribute('color', "cyan");
-      });
-      
-      el.addEventListener('mouseleave', function () {
-        cursor.setAttribute('color', "#FF5733");
-      });
+    el.addEventListener('mouseenter', function () {
+      if (!all_selected.includes(objeto)) cursor.setAttribute('color', "green");
+      else cursor.setAttribute('color', "cyan");
+    });
 
+    el.addEventListener('mouseleave', function () {
+      cursor.setAttribute('color', "#FF5733");
+    });
 
-      el.addEventListener('click',function(){
-        let in_action = false;
-        if(a[objeto]==true){ //verificar se é objeto
-          a[objeto]=false;
-          all_selected.push(objeto);
-          count_found++;
-          if (count_found == Object.keys(a).length){
-            all_found = true;
-          } 
+    el.addEventListener('click', function () {
+      let in_action = false;
+      if (a[objeto] == true) { // objeto por encontrar
+        a[objeto] = false;
+        all_selected.push(objeto);
+        count_found++;
+        if (count_found == Object.keys(a).length) {
+          all_found = true;
         }
-        else {
-          for (let key in acoes) {
-            if (acoes[key].includes(objeto)) {
-              acoes[key] = acoes[key].filter(item => item != objeto);
-              all_selected.push(objeto);
-              in_action = true;
-              if (acoes[key].length === 0) {
-                count_done++;
-                if (count_done === Object.keys(acoes).length) {
-                  all_done = true;
-                }
+      } else {                 // parte de ação por realizar 
+        for (let key in acoes) {
+          if (acoes[key].includes(objeto)) {
+            acoes[key] = acoes[key].filter(item => item != objeto);
+            all_selected.push(objeto);
+            in_action = true;
+            if (acoes[key].length === 0) {
+              count_done++;
+              if (count_done === Object.keys(acoes).length) {
+                all_done = true;
               }
             }
           }
         }
-        if (a[objeto]==false || in_action) {
-          let concatenatedString = "Palavras:" + "\n";
-            for (const key in a) {
-                if (a[key]) {
-                    concatenatedString += "o - ";
-                }
-                else {
-                    concatenatedString += "x - ";
-                }
-                concatenatedString += key + "\n";
-            }
-            concatenatedString += "---------------\n";
-            concatenatedString += "Tarefas:\n";
-            for (let key in acoes) {
-              let percentage = (1-(acoes[key].length/2))*100;
-              concatenatedString += percentage.toString() + "%: " + key + "\n";
-              for (let i = 0; i < acoes[key].length; i++) {
-                concatenatedString += ' -> ' + acoes[key][i] + '\n';
-              }
-            }
-            list_component.setAttribute('value', concatenatedString);
-          }
-      })
-    }
-  });
+      }
+      if (a[objeto] == false || in_action) {
+        updateList(); // apenas se mudou algo
+      }
+    })
+  }
+});
 
-// Initialize variables
-// Function to update the timer text
+function toggleVisibility() {
+  var myBox = document.getElementById('myBox');
+  myBox.getAttribute('visible') ? myBox.setAttribute('visible', false) : myBox.setAttribute('visible', true);
+}
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'l' || event.key === 'L') {
+    toggleVisibility();
+  }
+});
+
+let second = 0; 
+let minute = 0;
+
 function updateTimer() {
   const timerText = document.getElementById('timer');
-
   let second_string = "0";
   let minute_string = "0";
   if (second < 10) second_string = second_string + second.toString();
@@ -129,29 +112,11 @@ function updateTimer() {
     minute++;
   }
 
-  // Check if your boolean variable is true
   if (all_found && all_done) {
-    clearTimeout(timerInterval); // Stop the timer
+    clearTimeout(timerInterval); 
     timerText.setAttribute('value', 'Complete! ' + timerText.getAttribute('value'));
     timerText.setAttribute('color', 'green');
   }
 }
 
-// Function to toggle visibility of the A-Frame entity
-function toggleVisibility() {
-var myBox = document.getElementById('myBox');
-myBox.getAttribute('visible') ? myBox.setAttribute('visible', false) : myBox.setAttribute('visible', true);
-}
-
-
-
-// Add event listener for the 'L' key press
-document.addEventListener('keydown', function (event) {
-  if (event.key === 'l' || event.key === 'L') {
-        toggleVisibility();
-  }
-});
-
-let second = 0; // Initial time in seconds
-let minute = 0;
 const timerInterval = setInterval(updateTimer, 1000);
