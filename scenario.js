6ac142ab27acc5a18f1b08cf77f7106a42563f90
samu <sebmachado2002@gmail.com>
2023-11-10 14:05:01 +0000

    sair b

--- scenario.js
+++ scenario.js
@@ -176,6 +176,37 @@ function toggleVisibility() {
   }
 }
 
+AFRAME.registerComponent('showConfirmation', {
+  init: function () {
+    var confirmationUI = document.getElementById('confirmationUI');
+    this.el.addEventListener('click', function () {
+      confirmationUI.setAttribute('visible', true);
+    });
+  }
+});
+
+AFRAME.registerComponent('confirmAction', {
+  init: function () {
+    var confirmationUI = document.getElementById('confirmationUI');
+    this.el.addEventListener('click', function () {
+      console.log('Confirmed!');
+      // Add your redirection logic here
+      window.location.href = './home.html';
+      confirmationUI.setAttribute('visible', false);
+    });
+  }
+});
+
+AFRAME.registerComponent('cancelAction', {
+  init: function () {
+    var confirmationUI = document.getElementById('confirmationUI');
+    this.el.addEventListener('click', function () {
+      console.log('Canceled!');
+      confirmationUI.setAttribute('visible', false);
+    });
+  }
+});
+
 
 // Add event listener for the 'L' key press
 document.addEventListener('abuttondown', function (event) {
@@ -185,11 +216,7 @@ document.addEventListener('abuttondown', function (event) {
 });
 
 document.addEventListener('bbuttondown', function (event) {
-  var userConfirmed = window.confirm('Queres mesmo sair?');
-
-  if (userConfirmed) {
-    window.location.href = './home.html';
-  }
+  window.location.href = './home.html';
  });
 
 let second = 0; // Initial time in seconds
